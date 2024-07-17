fc081cc7330ad1b337ecfca3d1305e504e70ffe9
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2024-07-17 18:26:37 +0800

    _preamble.mk

---
+++ _preamble.mk
@@ -0,0 +1,17 @@
+SHELL := /bin/bash
+MAKEFLAGS += --warn-undefined-variables
+MAKEFLAGS += --no-builtin-variables
+MAKEFLAGS += --no-builtin-rules
+.DELETE_ON_ERROR:
+.SECONDARY:
+.SECONDEXPANSION:
+
+ifeq (3.82,$(firstword $(sort $(MAKE_VERSION) 3.82)))
+$(warning Makefile written with version 3.81 (bundled with macos sonoma 14.1), running with version $(MAKE_VERSION))
+$(warning New features like .ONESHELL .SHELLFLAGS .RECIPEPREFIX can be used, see: https://lists.gnu.org/archive/html/info-gnu/2010-07/msg00023.html)
+# .SHELLFLAGS := -o errexit -o nounset -o pipefail -c
+# .ONESHELL:
+# .RECIPEPREFIX := >
+endif
+
+# .DEFAULT_GOAL := default
